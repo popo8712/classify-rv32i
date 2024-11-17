e281e512e777963080fa4f8853442406ff2acf2c
popo8712 <161846035+popo8712@users.noreply.github.com>
2024-11-17 19:15:27 +0800

    Update dot.s

--- src/dot.s
+++ src/dot.s
@@ -32,13 +32,31 @@ dot:
     blt a4, t0, error_terminate  
 
     li t0, 0            
-    li t1, 0         
+    li t1, 0
+    slli a3, a3, 2 # a0's skip distamce
+    slli a4, a4, 2 # a1's skip distance         
 
 loop_start:
-    bge t1, a2, loop_end
-    # TODO: Add your own implementation
+    lw t2, 0(a0)
+    lw t3, 0(a1)
 
-loop_end:
+multiply_loop:
+    beq t3, x0, multiply_end    # If t3 == 0, jump to the end of the loop
+    andi t5, t3, 1              # Check the least significant bit of t3 and the result stored in t5
+    beq t5, x0, skip_add        # If the least significant bit is 0, skip addition
+    add t0, t0, t2              # Add t2 to the result stored in t0
+
+skip_add:
+    slli t2, t2, 1              # Left shift t2 by one bit (equivalent to multiplying by 2)
+    srli t3, t3, 1              # Right shift t3 by one bit (equivalent to dividing by 2)
+    j multiply_loop             # Jump back to the beginning of the loop
+
+multiply_end:
+    add a0, a0, a3     # a0's strides
+    add a1, a1, a4     # a1's strides 
+    addi a2, a2, -1    # a2 --
+    blt t1, a2, loop_start
+
     mv a0, t0
     jr ra
 
